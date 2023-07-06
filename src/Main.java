e3e5f2bf375605c6eb08f6637a4118eb7d2054be
Andri1i <dayzlopata05@gmail.com>
2023-07-06 17:17:03 +0100

    HomeWork4_1

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+
+        String myVariable = "RobotDreams";
+        System.out.println(myVariable.toUpperCase()); // Try_
+        System.out.println(myVariable.toLowerCase()); // Try)(
+
+        System.out.println(myVariable.charAt(0)); // Спроба_
+        System.out.println(myVariable.charAt(myVariable.length()-1)); // Спроба)(
+
+    }
+}
